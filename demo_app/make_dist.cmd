7c57e696055573aa7ada9833770aa05da943a9e3
59de44955ebd <59de44955ebd@gmail.com>
2025-11-08 14:20:40 +0100

    added GUI demo app

--- demo_app/make_dist.cmd
+++ demo_app/make_dist.cmd
@@ -26,6 +26,8 @@ del "dist\%APP_NAME%\_internal\_socket.pyd"
 del "dist\%APP_NAME%\_internal\libcrypto-3.dll"
 del "dist\%APP_NAME%\_internal\select.pyd"
 
+call :create_7z
+
 :done
 echo.
 echo ****************************************
@@ -33,3 +35,25 @@ echo Done.
 echo ****************************************
 echo.
 pause
+
+endlocal
+goto :eof
+
+
+:create_7z
+if not exist "C:\Program Files\7-Zip\" (
+	echo.
+	echo ****************************************
+	echo 7z.exe not found at default location, omitting .7z creation...
+	echo ****************************************
+	exit /B
+)
+echo.
+echo ****************************************
+echo Creating .7z archive...
+echo ****************************************
+cd dist
+set PATH=C:\Program Files\7-Zip;%PATH%
+7z a "%APP_NAME%-windows-x64.7z" "%APP_NAME%\*"
+cd ..
+exit /B
